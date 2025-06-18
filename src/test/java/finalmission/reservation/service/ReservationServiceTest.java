8d3c1f2c50fa41edf00e48ad95362ca493fee801
wogus <ind07152@naver.com>
2025-06-18 15:26:45 +0900

    feat: 예약 성공 시 이메일 발신 추가

--- src/test/java/finalmission/reservation/service/ReservationServiceTest.java
+++ src/test/java/finalmission/reservation/service/ReservationServiceTest.java
@@ -1,14 +1,22 @@
 package finalmission.reservation.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import finalmission.reservation.dto.request.CreateReservationRequest;
+import finalmission.reservation.dto.response.CreateReservationResponse;
+import finalmission.reservation.infrastructure.email.EmailSender;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.jdbc.Sql;
 
 @Import(ReservationService.class)
@@ -18,6 +26,9 @@ class ReservationServiceTest {
 
     @Autowired
     private ReservationService reservationService;
+
+    @MockitoBean
+    private EmailSender emailSender;
 
     @Test
     @DisplayName("예약 생성시 존재하지 않은 맴버 아이디가 들어오면 예외가 발생한다.")
@@ -55,4 +66,19 @@ class ReservationServiceTest {
                 .hasMessage("해당 시간에 예약이 존재합니다.");
     }
 
+    @Test
+    @DisplayName("성공적으로 예약을 생성한다.")
+    void creatReservation_success() {
+        // given
+        LocalDate reservationDate = LocalDate.now().plusDays(1);
+        CreateReservationRequest request = new CreateReservationRequest(reservationDate, 1L);
+        Long loginId = 1L;
+        // when
+        CreateReservationResponse response = reservationService.createReservation(request, loginId);
+        // then
+        verify(emailSender, times(1)).sendSuccessEmail(any(), any(), any());
+        assertThat(response.username()).isEqualTo("cogi");
+        assertThat(response.date()).isEqualTo(reservationDate);
+        assertThat(response.time()).isEqualTo(LocalTime.of(10, 0));
+    }
 }
